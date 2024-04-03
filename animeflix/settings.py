65e1c985bb31e145ed3fde53888a6b79c14f40a5
RatingTax78753 <ruanvr2006@gmail.com>
2024-04-03 10:21:05 -0300

    Debug = False

--- animeflix/settings.py
+++ animeflix/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w4e(fnwb3^$=+^v-))xhp^e32sak7hjcr*b$c$ht!owr-vgb^z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
