efb7935b472164140afbb780f618f5dee6941a48
RatingTax78753 <ruanvr2006@gmail.com>
2024-05-17 10:47:38 -0300

    versao 1.51

--- animeflix/settings.py
+++ animeflix/settings.py
@@ -138,9 +138,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-MEDIA_URL = 'media/'
+MEDIA_URL = 'imagens/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR / "media/")
+MEDIA_ROOT = os.path.join(BASE_DIR / "static/images/anime/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
